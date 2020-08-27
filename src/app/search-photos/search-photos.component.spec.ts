b8fa37ff6833601d1589aeb8eff4b6e99854fb62
Hevski <heathercmalloch@gmail.com>
2020-08-27 16:12:45 +0100

    created search component

---
+++ src/app/search-photos/search-photos.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchPhotosComponent } from './search-photos.component';
+
+describe('SearchPhotosComponent', () => {
+  let component: SearchPhotosComponent;
+  let fixture: ComponentFixture<SearchPhotosComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SearchPhotosComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchPhotosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/search-photos/search-photos.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-photos',
+  templateUrl: './search-photos.component.html',
+  styleUrls: ['./search-photos.component.css']
+})
+export class SearchPhotosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  search() {
+    console.log('hello')
+  }
+
+}
